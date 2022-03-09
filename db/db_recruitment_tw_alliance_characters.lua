5df450b3c531424f4b0949a420425858f862010f
Jah Raphael <jahraphael@yahoo.com>
2022-03-09 06:32:52 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_alliance_characters.lua
+++ db/db_recruitment_tw_alliance_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",faction=1,date="2022-03-08T06:16:51Z",numCharacters=21286,db1={}}
+local provider={name=...,data=3,region="tw",faction=1,date="2022-03-09T06:31:32Z",numCharacters=21444,db1={}}
 local F
 
 F = function() provider.db1["暗影之月"]={0,"拾九"} end F()
--- db/db_recruitment_tw_horde_lookup.lua
+++ db/db_recruitment_tw_horde_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",faction=2,date="2022-03-08T06:16:51Z",numCharacters=21286,lookup2={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",faction=2,date="2022-03-09T06:31:32Z",numCharacters=21444,lookup2={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 10
