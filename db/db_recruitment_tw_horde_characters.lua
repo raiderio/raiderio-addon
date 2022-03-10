e5e17621e924ad163877879ddb9e6a180b24e74b
Jah Raphael <jahraphael@yahoo.com>
2022-03-10 06:32:26 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_horde_characters.lua
+++ db/db_recruitment_tw_horde_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",faction=2,date="2022-03-09T06:31:32Z",numCharacters=21444,db2={}}
+local provider={name=...,data=3,region="tw",faction=2,date="2022-03-10T06:31:06Z",numCharacters=21449,db2={}}
 local F
 
 F = function() provider.db2["寒冰皇冠"]={0,"Elvemigchat","也只是怕錯過"} end F()
