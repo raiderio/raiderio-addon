6828b46f0d3f4d106db718870d2cc382e740d2ae
Jah Raphael <jahraphael@yahoo.com>
2022-03-14 06:46:18 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_alliance_characters.lua
+++ db/db_recruitment_kr_alliance_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",faction=1,date="2022-03-13T06:32:49Z",numCharacters=22040,db1={}}
+local provider={name=...,data=3,region="kr",faction=1,date="2022-03-14T06:43:28Z",numCharacters=21815,db1={}}
 local F
 
 
--- db/db_recruitment_kr_horde_characters.lua
+++ db/db_recruitment_kr_horde_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",faction=2,date="2022-03-13T06:32:49Z",numCharacters=22040,db2={}}
+local provider={name=...,data=3,region="kr",faction=2,date="2022-03-14T06:43:28Z",numCharacters=21815,db2={}}
 local F
 
 F = function() provider.db2["아즈샤라"]={0,"회드새끼"} end F()
