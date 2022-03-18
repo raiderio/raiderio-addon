5a22c561c2fb234103abd09dd6c8439b2729e69c
Jah Raphael <jahraphael@yahoo.com>
2022-03-18 06:51:42 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_alliance_characters.lua
+++ db/db_recruitment_kr_alliance_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",faction=1,date="2022-03-17T06:47:00Z",numCharacters=21852,db1={}}
+local provider={name=...,data=3,region="kr",faction=1,date="2022-03-18T06:47:16Z",numCharacters=21719,db1={}}
 local F
 
 
--- db/db_recruitment_kr_horde_lookup.lua
+++ db/db_recruitment_kr_horde_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2022 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",faction=2,date="2022-03-17T06:47:00Z",numCharacters=21852,lookup2={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="kr",faction=2,date="2022-03-18T06:47:16Z",numCharacters=21719,lookup2={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
