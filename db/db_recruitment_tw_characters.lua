53518539220b793d24ac25d30c2390a07cfcdbb3
Jah Raphael <jahraphael@yahoo.com>
2023-12-09 09:14:56 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-12-08T08:56:58Z",numCharacters=31229,db={}}
+local provider={name=...,data=3,region="tw",date="2023-12-09T09:06:59Z",numCharacters=31446,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Driftsand","Momococo","低排放載具","卡錫恩","吃土更健康","地獄吹水","大叔和理非","天線低低嗶","小龍公主","惡魔小妹妹","流砂","浪潮披薩","減弱薩滿","科興大師兄","芋泥","阿耳忒弥斯","黑魔后"} end F()
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-12-08T08:56:58Z",numCharacters=31229,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-12-09T09:06:59Z",numCharacters=31446,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 38
