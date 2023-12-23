00aaad5a0afb654c53401ed694f3a63d780ba9ad
Jah Raphael <jahraphael@yahoo.com>
2023-12-23 09:42:25 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,16 +2,16 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-12-22T09:33:11Z",numCharacters=30561,db={}}
+local provider={name=...,data=3,region="tw",date="2023-12-23T09:31:09Z",numCharacters=30493,db={}}
 local F
 
 F = function() provider.db["冰風崗哨"]={0,"保温杯放可乐","公主请坐牢","冰歧淋","喵貓貓喵貓貓","妳幹嘛哎喲","戀夏","戀夏丶","我超威的","战斗暴龙兽","斷了綫的風箏","系艾玛","索尔","繁花如愿","聖光照耀尾巴","薩拉塔斯姐姐","踢皮球的小哥","逮蝦戶","酒館發牌員"} end F()
 F = function() provider.db["水晶之刺"]={36,"咕咕吖"} end F()
 F = function() provider.db["聖光之願"]={38,"微醺灬墨染曦"} end F()
 F = function() provider.db["天空之牆"]={40,"貓叔的小耳朵","貓叔的小肉球","貓叔的小鈴鐺"} end F()
-F = function() provider.db["暗影之月"]={46,"Driftsand","流砂","紫紫菜","聆听寒冬"} end F()
-F = function() provider.db["米奈希爾"]={54,"雪鹽焦糖貓"} end F()
-F = function() provider.db["眾星之子"]={56,"聆听寒冬"} end F()
+F = function() provider.db["暗影之月"]={46,"Driftsand","奧術智慧","流砂","紫紫菜","聆听寒冬"} end F()
+F = function() provider.db["米奈希爾"]={56,"雪鹽焦糖貓"} end F()
+F = function() provider.db["眾星之子"]={58,"聆听寒冬"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-12-22T09:33:11Z",numCharacters=30561,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2023-12-23T09:31:09Z",numCharacters=30493,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 58
-F = function() provider.lookup[1] = "?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5;\4;\4\169\30;\4\12\8;\4" end F()
+-- chunk size: 60
+F = function() provider.lookup[1] = "?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5?\5;\4;\4;\4\169\30;\4\12\8;\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
