96eef1d3afea74146b09786d8835b4f0e5fafb78
Jah Raphael <jahraphael@yahoo.com>
2023-12-31 20:10:02 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_lookup.lua
+++ db/db_recruitment_kr_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2023-12-30T09:41:20Z",numCharacters=28115,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="kr",date="2023-12-31T19:57:08Z",numCharacters=27588,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2023-12-30T09:41:20Z",numCharacters=28115,db={}}
+local provider={name=...,data=3,region="tw",date="2023-12-31T19:57:08Z",numCharacters=27588,db={}}
 local F
 
 F = function() provider.db["冰風崗哨"]={0,"保温杯放可乐","公主请坐牢","冰歧淋","喵貓貓喵貓貓","妳幹嘛哎喲","戀夏","戀夏丶","我超威的","战斗暴龙兽","斷了綫的風箏","系艾玛","索尔","繁花如愿","聖光照耀尾巴","薩拉塔斯姐姐","踢皮球的小哥","逮蝦戶","酒館發牌員"} end F()
@@ -10,8 +10,7 @@ F = function() provider.db["水晶之刺"]={36,"咕咕吖","聆听寒冬"} end F
 F = function() provider.db["聖光之願"]={40,"微醺灬墨染曦"} end F()
 F = function() provider.db["天空之牆"]={42,"貓叔的小耳朵","貓叔的小肉球","貓叔的小鈴鐺"} end F()
 F = function() provider.db["暗影之月"]={48,"Driftsand","奧術智慧","流砂","紫紫菜","聆听寒冬"} end F()
-F = function() provider.db["米奈希爾"]={58,"雪鹽焦糖貓"} end F()
-F = function() provider.db["銀翼要塞"]={60,"浅碧轻红"} end F()
+F = function() provider.db["銀翼要塞"]={58,"浅碧轻红"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
