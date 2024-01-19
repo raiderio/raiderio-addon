bdfc1644d87f8ac97b2c72ae9b65f3ba8c92d5a2
Jah Raphael <jahraphael@yahoo.com>
2024-01-19 09:43:06 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,20 +2,20 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-01-18T09:14:15Z",numCharacters=27925,db={}}
+local provider={name=...,data=3,region="tw",date="2024-01-19T09:18:18Z",numCharacters=27817,db={}}
 local F
 
-F = function() provider.db["語風"]={0,"Loadingpr","Loadingzz","Panpanda","上汤娃娃菜","不可能更快乐","伊甸園的笨熊","你劈我瓜是吧","双生之焰","吃饭睡觉","吾乃惠惠","和風","哲别","国服之光","坎特","大水牛丶","大瓜","小奶牛","少主不要停","少主快来嘛","情话还是说好","感覺到","是恢恢","東東哥哥","東雲名乃","水頭媽媽桑","深藍爺叔","灬水中月灬","牛肉糊","老公","落夢夢","蜜柚小騎","这回真寄咯","釋丶迦瀾達","陳鼎鼎睡覺嘍","青芒","静电小子","鱼没有脚印"} end F()
-F = function() provider.db["阿薩斯"]={74,"Eapleue","兔牧","只為心中蒼穹"} end F()
-F = function() provider.db["寒冰皇冠"]={80,"敷衍丨神","花月詩酒茶"} end F()
-F = function() provider.db["暗影之月"]={84,"傻傻的笨图图","心丶动","沙雕之杖","肆依贼","蘋菓","蘋菓丶","貓貓奶凰","高板桐乃"} end F()
-F = function() provider.db["冰風崗哨"]={100,"丨姜小白丨","叁一","情歌还是唱好"} end F()
-F = function() provider.db["尖石"]={106,"韩住"} end F()
-F = function() provider.db["世界之樹"]={108,"Aespa","Karinadr","Kiily","丶衍","卩东木交長","卩东校长","卩東木交長","卩東校长","烟雪花莹","花寳","阝东校长","阝東木交長","阝東木交长","阝東校长","阿偉羅","陈木交長","陳木交長","陳木交长","陳校长","霸气太重"} end F()
-F = function() provider.db["銀翼要塞"]={148,"浅碧轻红"} end F()
-F = function() provider.db["日落沼澤"]={150,"Tomo","吐無"} end F()
-F = function() provider.db["米奈希爾"]={154,"雪鹽焦糖貓"} end F()
-F = function() provider.db["憤怒使者"]={156,"傲寒","機械暴龍獸"} end F()
+F = function() provider.db["語風"]={0,"Loadingpr","Loadingzz","Panpanda","上汤娃娃菜","不可能更快乐","伊甸園的笨熊","你劈我瓜是吧","切赫","双生之焰","吃饭睡觉","听风起雨落","吾乃惠惠","和風","哲别","国服之光","坎特","大水牛丶","大瓜","小奶牛","少主不要停","少主快来嘛","情话还是说好","感覺到","是恢恢","東東哥哥","東雲名乃","水頭媽媽桑","深藍爺叔","灬水中月灬","牛肉糊","老公","落夢夢","蜜柚小騎","这回真寄咯","釋丶迦瀾達","陳鼎鼎睡覺嘍","青芒","静电小子","鱼没有脚印"} end F()
+F = function() provider.db["阿薩斯"]={78,"Eapleue","兔牧","只為心中蒼穹"} end F()
+F = function() provider.db["寒冰皇冠"]={84,"敷衍丨神","花月詩酒茶"} end F()
+F = function() provider.db["暗影之月"]={88,"傻傻的笨图图","心丶动","沙雕之杖","肆依贼","蘋菓","蘋菓丶","貓貓奶凰","高板桐乃"} end F()
+F = function() provider.db["冰風崗哨"]={104,"丨姜小白丨","叁一","情歌还是唱好"} end F()
+F = function() provider.db["尖石"]={110,"韩住"} end F()
+F = function() provider.db["世界之樹"]={112,"Aespa","Karinadr","Kiily","丶衍","卩东木交長","卩东校长","卩東木交長","卩東校长","烟雪花莹","花寳","阝东校长","阝東木交長","阝東木交长","阝東校长","阿偉羅","陈木交長","陳木交長","陳木交长","陳校长","霸气太重"} end F()
+F = function() provider.db["銀翼要塞"]={152,"浅碧轻红"} end F()
+F = function() provider.db["日落沼澤"]={154,"Tomo","吐無"} end F()
+F = function() provider.db["米奈希爾"]={158,"雪鹽焦糖貓"} end F()
+F = function() provider.db["憤怒使者"]={160,"傲寒","機械暴龍獸"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
