07196a683f8af28c6e4be736aee64904076ab33e
Jah Raphael <jahraphael@yahoo.com>
2024-01-25 09:39:20 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-01-24T09:19:06Z",numCharacters=28320,db={}}
+local provider={name=...,data=3,region="tw",date="2024-01-25T09:24:08Z",numCharacters=28556,db={}}
 local F
 
 F = function() provider.db["語風"]={0,"Loadingpr","Loadingzz","Panpanda","上汤娃娃菜","不可能更快乐","伊甸園的笨熊","你劈我瓜是吧","切赫","双生之焰","吃饭睡觉","听风起雨落","吾乃惠惠","和風","哲别","国服之光","坎特","大水牛丶","大瓜","小奶牛","少主不要停","少主快来嘛","情话还是说好","感覺到","是恢恢","東東哥哥","東雲名乃","水頭媽媽桑","深藍爺叔","灬水中月灬","牛肉糊","老公","落夢夢","蜜柚小騎","这回真寄咯","釋丶迦瀾達","陳鼎鼎睡覺嘍","青芒","静电小子","鱼没有脚印"} end F()
@@ -12,8 +12,8 @@ F = function() provider.db["暗影之月"]={86,"傻傻的笨图图","心丶动",
 F = function() provider.db["冰風崗哨"]={102,"丨姜小白丨","叁一","情歌还是唱好"} end F()
 F = function() provider.db["尖石"]={108,"韩住"} end F()
 F = function() provider.db["世界之樹"]={110,"Aespa","Karinadr","Kiily","丶衍","卩东木交長","卩东校长","卩東木交長","卩東校长","烟雪花莹","花寳","阝东校长","阝東木交長","阝東木交长","阝東校长","阿偉羅","陈木交長","陳木交長","陳木交长","陳校长","霸气太重"} end F()
-F = function() provider.db["米奈希爾"]={150,"雪鹽焦糖貓"} end F()
-F = function() provider.db["憤怒使者"]={152,"傲寒","機械暴龍獸"} end F()
+F = function() provider.db["憤怒使者"]={150,"傲寒","機械暴龍獸"} end F()
+F = function() provider.db["米奈希爾"]={154,"雪鹽焦糖貓"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2023 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-01-24T09:19:06Z",numCharacters=28320,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2024-01-25T09:24:08Z",numCharacters=28556,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 156
-F = function() provider.lookup[1] = "<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29;\4<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29@\13@\13s\29@\13s\29s\29s\29s\29@\13@\13s\29s\29s\29s\29@\13s\29s\29s\29s\29\8\4\8\16\4\4\4\4" end F()
+F = function() provider.lookup[1] = "<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29;\4<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29<\29@\13@\13s\29@\13s\29s\29s\29s\29@\13@\13s\29s\29s\29s\29@\13s\29s\29s\29s\29\8\4\4\4\4\4\8\16" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
