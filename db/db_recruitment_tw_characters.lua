907b4bc23f3486537a6cf69a34781fd380b2e394
Jah Raphael <jahraphael@yahoo.com>
2024-12-29 07:23:03 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-12-28T07:14:05Z",numCharacters=24944,db={}}
+local provider={name=...,data=3,region="tw",date="2024-12-29T07:15:20Z",numCharacters=24500,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Ggpaladin"} end F()
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2024 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2024-12-28T07:14:05Z",numCharacters=24944,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2024-12-29T07:15:20Z",numCharacters=24500,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 2
