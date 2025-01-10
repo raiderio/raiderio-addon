2958af5250e05f8c2236a1294854bc9e247e6dc3
Jah Raphael <jahraphael@yahoo.com>
2025-01-10 06:08:14 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-01-09T06:08:16Z",numCharacters=22,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2025-01-10T06:07:32Z",numCharacters=22,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 8
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-01-09T06:08:16Z",numCharacters=22,db={}}
+local provider={name=...,data=3,region="us",date="2025-01-10T06:07:32Z",numCharacters=22,db={}}
 local F
 
 F = function() provider.db["Benediction"]={0,"Abbilene","Abbilify","Abbillanica","Abbizolam","Abbriella","Daestronous","Daestrous","Focalin","Vaerynd"} end F()
