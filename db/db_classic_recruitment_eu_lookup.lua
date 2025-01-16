bacc334c66f25fd65ee52ec24737cf6c9a393bac
Jah Raphael <jahraphael@yahoo.com>
2025-01-16 06:09:38 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-01-15T06:09:00Z",numCharacters=26,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2025-01-16T06:09:00Z",numCharacters=26,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 8
