7398adeb345aa581f97e571c5be25acf8db600f9
Jah Raphael <jahraphael@yahoo.com>
2025-01-28 08:34:26 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2025-01-27T08:38:25Z",numCharacters=27031,db={}}
+local provider={name=...,data=3,region="kr",date="2025-01-28T08:26:42Z",numCharacters=27346,db={}}
 local F
 
-F = function() provider.db["아즈샤라"]={0,"Icepink","루브루브","모카기사님","모카아이스님","모카악사님","모카죽기님","쇽쇽전사","술사모카","와우레이드톡방","잠탱이루브","잠탱이루브님"} end F()
-F = function() provider.db["세나리우스"]={22,"궤반드립","부엉부엉부엉이","토석토석토석인"} end F()
+F = function() provider.db["아즈샤라"]={0,"Icepink","루브루브","모카기사님","모카아이스님","모카악사님","모카죽기님","쇽쇽전사","술사모카","아이스블루베리","와우레이드톡방","잠탱이루브","잠탱이루브님","쿠로베리"} end F()
+F = function() provider.db["세나리우스"]={26,"궤반드립","부엉부엉부엉이","토석토석토석인"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
