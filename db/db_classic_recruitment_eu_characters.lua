9bab6f23467467ba0212b838f5b36790f3cce023
Jah Raphael <jahraphael@yahoo.com>
2025-01-29 06:11:21 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,11 +2,10 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-01-28T06:10:17Z",numCharacters=17,db={}}
+local provider={name=...,data=3,region="eu",date="2025-01-29T06:11:03Z",numCharacters=34,db={}}
 local F
 
 F = function() provider.db["Flamegor"]={0,"Медифв"} end F()
-F = function() provider.db["Golemagg"]={2,"Sugynopr"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-01-28T06:10:17Z",numCharacters=17,db={}}
+local provider={name=...,data=3,region="us",date="2025-01-29T06:11:03Z",numCharacters=34,db={}}
 local F
 
-F = function() provider.db["Whitemane"]={0,"Bigeasy"} end F()
-F = function() provider.db["Mankrik"]={2,"Baitandtackl","Blazem","Dkeo","Drulino","Fleshharvest","Galacnocok","Kadron","Message","Sneox","Tyder","Tydesin","Tydison","Viscerill","Weast"} end F()
+F = function() provider.db["Whitemane"]={0,"Bigdangerous","Gannx","Pickling","Schnizz"} end F()
+F = function() provider.db["Mankrik"]={8,"Altere","Cawanu","Dknold","Elder","Elderne","Eldertoo","Elementalino","Galacnocok","Habiibi","Huntinnuggs","Kadron","Kèrupt","Lazzarrass","Limz","Lymz","Mon","Neroslice","Nevlak","Osakha","Revirez","Shadowgrave","Shotalotapus","Sinthra","Slontsdk","Styggmz","Twistedfaith","Tyder","Varltok","Zaritjr"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
