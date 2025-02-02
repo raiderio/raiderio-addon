80b1ffc62de686e0eae4cc58b3cdf6b24f33f64b
Jah Raphael <jahraphael@yahoo.com>
2025-02-02 08:30:25 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2025-02-01T08:24:53Z",numCharacters=27529,db={}}
+local provider={name=...,data=3,region="tw",date="2025-02-02T08:23:26Z",numCharacters=27307,db={}}
 local F
 
 F = function() provider.db["聖光之願"]={0,"一霽無瑕一","光頭暴擊高","兩儀織","叫爸爸","孜然小酥肉","新木優子","紅燒獅子頭","維魯德拉","艾德吻","艾德妮","艾德林","艾德狼","艾德莉亞","雪貓甜滋滋"} end F()
