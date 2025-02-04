50713d9410bd17f1a6adb1fdd141ddc49a0ab26a
Jah Raphael <jahraphael@yahoo.com>
2025-02-04 08:24:58 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2025-02-03T08:22:31Z",numCharacters=27158,db={}}
+local provider={name=...,data=3,region="kr",date="2025-02-04T08:18:06Z",numCharacters=27570,db={}}
 local F
 
-F = function() provider.db["아즈샤라"]={0,"Icepink","루브루브","모카기사님","모카아이스님","모카악사님","모카죽기님","쇽쇽전사","술사모카","잠탱이루브","잠탱이루브님","쿠로베리"} end F()
-F = function() provider.db["세나리우스"]={22,"궤반드립","부엉부엉부엉이","토석토석토석인"} end F()
+F = function() provider.db["아즈샤라"]={0,"Icepink","루브루브","모카기사님","모카아이스님","모카악사님","모카죽기님","쇽쇽전사","술사모카","와우레이드톡방","잠탱이루브","잠탱이루브님","쿠로베리"} end F()
+F = function() provider.db["세나리우스"]={24,"궤반드립","부엉부엉부엉이","토석토석토석인"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2025-02-03T08:22:31Z",numCharacters=27158,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2025-02-04T08:18:06Z",numCharacters=27570,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 40
