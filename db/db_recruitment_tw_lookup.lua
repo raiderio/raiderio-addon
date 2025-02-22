f3100ef0186463af5031730513d7cfdfd0305215
Jah Raphael <jahraphael@yahoo.com>
2025-02-22 07:50:28 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_lookup.lua
+++ db/db_recruitment_tw_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2025-02-21T07:45:06Z",numCharacters=30309,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="tw",date="2025-02-22T07:42:31Z",numCharacters=30347,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 40
-F = function() provider.lookup[1] = "?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29" end F()
+-- chunk size: 42
+F = function() provider.lookup[1] = "?\29t\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29?\29" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_score_tiers.lua
+++ db/db_score_tiers.lua
@@ -1,5 +1,5 @@
 --
--- Generated on 2025-02-21T07:49:09Z. DO NOT EDIT.
+-- Generated on 2025-02-22T07:46:38Z. DO NOT EDIT.
 --
 -- Ranges: {"epic":[2676,3725],"superior":[2401,2675],"uncommon":[1151,2400],"common":[200,1150]}
 --
