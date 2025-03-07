6f2e40c07ca52ac858c37ed356755032c3f98bd8
Jah Raphael <jahraphael@yahoo.com>
2025-03-07 06:06:49 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,12 +2,12 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-03-06T06:05:00Z",numCharacters=98,db={}}
+local provider={name=...,data=3,region="us",date="2025-03-07T06:06:06Z",numCharacters=91,db={}}
 local F
 
 F = function() provider.db["Benediction"]={0,"Avesantana","Custoslucis","Dkarthasdk","Dktankdk","Dártos","Fantacasual","Ghalwise","Herefohero","Incindy","Naahdeadass","Samsdruid","Sapporah","Stokedmyth","Tinkmeister"} end F()
-F = function() provider.db["Mankrik"]={28,"Altere","Caketank","Cawanu","Dknold","Dopåmine","Dromhunter","Dromlock","Drommekage","Droodkage","Drömmekage","Drømmekage","Elder","Elderne","Eldertoo","Elementalino","Gøkù","Habiibi","Howyadóinmon","Kadron","Kèrupt","Lazzarrass","Liljessie","Litler","Lymz","Låymon","Maikeruu","Manlycupcake","Mon","Neroslice","Nevlak","Osakha","Phailme","Pwniszher","Pølsemix","Revirez","Shadowgrave","Shotalotapus","Sinthra","Stitchyboi","Styggmz","Tyder","Varltok","Vinsmøke","Zaritjr","Zubee","Zuubedoo","Zuubinator","Zúzù","Èpinephrine"} end F()
-F = function() provider.db["Faerlina"]={126,"Blasteleon"} end F()
+F = function() provider.db["Mankrik"]={28,"Altere","Cawanu","Dknold","Dopåmine","Dromhunter","Dromlock","Drommekage","Droodkage","Drömmekage","Drømmekage","Elder","Elderne","Eldertoo","Elementalino","Galacnocok","Gøkù","Habiibi","Howyadóinmon","Kadron","Kèrupt","Lazzarrass","Liljessie","Litler","Lymz","Låymon","Mon","Moo","Mädd","Neroslice","Nevlak","Osakha","Pwniszher","Pølsemix","Randomshift","Rev","Revirez","Shadowgrave","Shotalotapus","Sinthra","Styggmz","Tyder","Varltok","Vinsmøke","Zaritjr","Èpinephrine"} end F()
+F = function() provider.db["Faerlina"]={118,"Blasteleon"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
