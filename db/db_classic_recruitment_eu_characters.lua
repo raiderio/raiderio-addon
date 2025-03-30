2145664f5ab975d4a8a2423278a3f8ec0db35988
Jah Raphael <jahraphael@yahoo.com>
2025-03-30 06:06:36 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-03-29T06:05:40Z",numCharacters=114,db={}}
+local provider={name=...,data=3,region="eu",date="2025-03-30T06:05:44Z",numCharacters=111,db={}}
 local F
 
 F = function() provider.db["Gehennas"]={0,"Armya","Bakaresh","Dwaaz","Elzaren","Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Gojosatoru","Gravelhunter","Gravelmage","Gravelsham","Hacezbank","Hugebenis","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Karamonn","Neprostodkk","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Sofeast","Trocks","Trockzie","Varggan","Venrora","Zachemm","Zugzuglady"} end F()
