b0aec3d6b793ecd138b26b5624e29211b65322a4
Jah Raphael <jahraphael@yahoo.com>
2025-04-15 06:07:48 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,15 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-04-14T06:06:44Z",numCharacters=129,db={}}
+local provider={name=...,data=3,region="eu",date="2025-04-15T06:06:51Z",numCharacters=124,db={}}
 local F
 
-F = function() provider.db["Gehennas"]={0,"Armya","Bakaresh","Disoulus","Dwaaz","Dwazzie","Elzaren","Epsteindrip","Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Gojosatoru","Hacezbank","Holymoleh","Hugebenis","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Karamonn","Mcflame","Mcgorlock","Mcoprah","Neprostodkk","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Sofeast","Trocks","Trockzie","Varggan","Vashavi","Venrora","Xiryfat","Zachemm","Zugzuglady"} end F()
-F = function() provider.db["PyrewoodVillage"]={106,"Amoebozoa","Auam","Elfren","Palrenn","Robdot","Roberino"} end F()
-F = function() provider.db["Flamegor"]={118,"Дормацайн","Кунанбаев","Миллифисент","Пуассон"} end F()
-F = function() provider.db["Auberdine"]={126,"Antipustule","Comburstion","Drips","Judgeret","Lumeniros","Lunou","Opey","Proclacondtm","Switchfords"} end F()
-F = function() provider.db["Venoxis"]={144,"Chagundk","Keldownz","Shibbyp","Zyonmythos"} end F()
-F = function() provider.db["Golemagg"]={152,"Avengingstab","Deftnight","Louisvuítton"} end F()
-F = function() provider.db["MirageRaceway"]={158,"Daveshock","Jodrun"} end F()
+F = function() provider.db["Gehennas"]={0,"Armya","Bakaresh","Disoulus","Dwaaz","Dwazzie","Elzaren","Epsteindrip","Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Gojosatoru","Hacezbank","Holymoleh","Hugebenis","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Karamonn","Mcflame","Mcgorlock","Mcoprah","Neprostodkk","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Sofeast","Trocks","Trockzie","Varggan","Vashavi","Venrora","Zachemm","Zugzuglady"} end F()
+F = function() provider.db["PyrewoodVillage"]={104,"Amoebozoa","Auam","Elfren","Palrenn","Robdot","Roberino"} end F()
+F = function() provider.db["Flamegor"]={116,"Дормацайн","Кунанбаев","Миллифисент","Пуассон"} end F()
+F = function() provider.db["Auberdine"]={124,"Antipustule","Comburstion","Drips","Judgeret","Lumeniros","Lunou","Opey","Proclacondtm","Switchfords"} end F()
+F = function() provider.db["Venoxis"]={142,"Chagundk","Keldownz","Shibbyp","Zyonmythos"} end F()
+F = function() provider.db["Golemagg"]={150,"Avengingstab","Deftnight","Louisvuítton"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-04-14T06:06:44Z",numCharacters=129,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2025-04-15T06:06:51Z",numCharacters=124,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 96
-F = function() provider.lookup[1] = "\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29" end F()
+-- chunk size: 92
+F = function() provider.lookup[1] = "\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
