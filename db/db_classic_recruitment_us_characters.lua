d02ccc97d1091b94c546033f08d99981f9b1418a
Jah Raphael <jahraphael@yahoo.com>
2025-04-23 06:12:52 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,12 +2,12 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-04-22T06:09:05Z",numCharacters=112,db={}}
+local provider={name=...,data=3,region="us",date="2025-04-23T06:11:42Z",numCharacters=113,db={}}
 local F
 
-F = function() provider.db["Mankrik"]={0,"Altere","Derisbear","Deriscorch","Derisdeath","Derisdots","Deriseal","Derishield","Derishock","Derishot","Derishout","Derismoo","Deristab","Deristas","Deristide","Deristorm","Deristree","Deristrike","Dknold","Eildear","Elder","Elderne","Eldertoo","Elementalino","Galacnocok","Habiibi","Kadron","Lazzarrass","Lymz","Mon","Neroslice","Osakha","Revirez","Shadowgrave","Shotalotapus","Sinthra","Styggmz","Varltok","Zaritjr"} end F()
-F = function() provider.db["Arugal"]={76,"Ayayayayayay","Bktd","Davelol","Davelolfan","Daveloli","Daveloltroll","Denkah","Denkar","Denkr","Dkdave","Fluffysbff","Fluffyshh","Fluffyshype","Fluffyspve","Fluffysum","Fluffyswl","Fluffyzz","Flxffys","Lylie","Notbucket","Photoncannon","Tashy","Txshy","Txshytwo"} end F()
-F = function() provider.db["Pagle"]={124,"Bubblebound","Ticklesand"} end F()
+F = function() provider.db["Mankrik"]={0,"Altere","Derisbear","Deriscorch","Derisdeath","Derisdots","Deriseal","Derishield","Derishock","Derishot","Derishout","Derismoo","Deristab","Deristas","Deristide","Deristorm","Deristree","Deristrike","Dknold","Eildear","Elder","Elderne","Eldertoo","Elementalino","Galacnocok","Habiibi","Kadron","Lazzarrass","Lymz","Magenold","Mon","Neroslice","Osakha","Revirez","Shadowgrave","Shotalotapus","Sinthra","Styggmz","Varltok","Zaritjr"} end F()
+F = function() provider.db["Arugal"]={78,"Ayayayayayay","Bktd","Davelol","Davelolfan","Daveloli","Daveloltroll","Denkah","Denkar","Denkr","Dkdave","Fluffysbff","Fluffyshh","Fluffyshype","Fluffyspve","Fluffysum","Fluffyswl","Fluffyzz","Flxffys","Lylie","Notbucket","Photoncannon","Tashy","Txshy","Txshytwo"} end F()
+F = function() provider.db["Pagle"]={126,"Bubblebound","Ticklesand"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
