8c412353f2e80895a1938475dc59c240244288d3
Jah Raphael <jahraphael@yahoo.com>
2025-05-10 06:10:17 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,14 +2,13 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-05-09T06:09:20Z",numCharacters=127,db={}}
+local provider={name=...,data=3,region="eu",date="2025-05-10T06:09:17Z",numCharacters=128,db={}}
 local F
 
 F = function() provider.db["Golemagg"]={0,"Seruss","Twism"} end F()
 F = function() provider.db["Gehennas"]={4,"Dwaaz","Dwazzie","Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Hacezbank","Holymoleh","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Mcflame","Mcgorlock","Mcoprah","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Trocks","Trockzie"} end F()
-F = function() provider.db["MirageRaceway"]={78,"Anurios","Aquaxxus","Astolffo","Baralia","Baraline","Baráth","Braldruid","Brathra","Brusekabine","Chaosjones","Deladin","Delfury","Delhancement","Drevani","Dýnem","Estouls","Faeden","Kalepp","Kyrielight","Legendfusion","Nishandra","Nyastolfo","Sanshein","Saphirar","Saphyron","Selenas","Shadylinkz","Taala","Tamamonomae","Teranosh","Theddios","Theddious","Thispala","Vezstrike","Whichpala","Whopala","Zamaas"} end F()
-F = function() provider.db["Auberdine"]={152,"Antistäminik","Lunou"} end F()
-F = function() provider.db["Venoxis"]={156,"Chagundk","Keldownz","Shibbyp","Zyonmythos"} end F()
+F = function() provider.db["MirageRaceway"]={78,"Anurios","Aquaxxus","Astolffo","Barahunts","Baralia","Baraline","Baráth","Braldruid","Brathra","Brusekabine","Chaosjones","Deladin","Delfury","Delhancement","Delshank","Drevani","Dýnem","Estouls","Faeden","Kalepp","Kyrielight","Legendfusion","Lethtendria","Mukunaru","Nishandra","Nyastolfo","Sanshein","Saphirar","Saphyron","Selenas","Shadylinkz","Taala","Tamamonomae","Teranosh","Theddios","Theddious","Thispala","Vezstrike","Whichpala","Whopala","Zamaas"} end F()
+F = function() provider.db["Auberdine"]={160,"Antistäminik","Lunou"} end F()
 F = function() provider.db["Firemaw"]={164,"Deksmydaddy","Luthtv","Warbny"} end F()
 
 F = nil
