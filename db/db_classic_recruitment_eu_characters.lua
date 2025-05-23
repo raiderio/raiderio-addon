006c48bc6453117a445950af933850441481c578
Jah Raphael <jahraphael@yahoo.com>
2025-05-23 06:10:33 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,14 +2,13 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-05-22T06:09:36Z",numCharacters=124,db={}}
+local provider={name=...,data=3,region="eu",date="2025-05-23T06:09:27Z",numCharacters=122,db={}}
 local F
 
-F = function() provider.db["Gehennas"]={0,"Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Goblinpung","Grandpá","Hacezbank","Holymoleh","Juben","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Loodycat","Loodydk","Loodyhunt","Loodylock","Loodymage","Loodypala","Loodypriest","Loodystab","Loodywar","Mcgorlock","Mcoprah","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Skillisue","Xencowmoo","Xentasia","Xën"} end F()
-F = function() provider.db["MirageRaceway"]={98,"Anurios","Aquaxxus","Astolffo","Barahunts","Baralia","Baraline","Baráth","Braldruid","Brathra","Brusekabine","Chaosjones","Deladin","Delfury","Delhancement","Delshank","Drevani","Dýnem","Estouls","Faeden","Kalepp","Kyrielight","Legendfusion","Loneta","Nishandra","Nyastolfo","Sanshein","Saphirar","Saphyron","Selenas","Shadylinkz","Taala","Tamamonomae","Teranosh","Theddios","Theddious","Thispala","Vezstrike","Whichpala","Whopala","Zamaas"} end F()
-F = function() provider.db["Auberdine"]={178,"Antistäminik","Lunou"} end F()
-F = function() provider.db["Firemaw"]={182,"Deksmydaddy","Luthtv"} end F()
-F = function() provider.db["Sulfuron"]={186,"Manakan","Taigâ"} end F()
+F = function() provider.db["Gehennas"]={0,"Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Goblinpung","Grandpá","Hacezbank","Holymoleh","Juben","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Loodycat","Loodydk","Loodyhunt","Loodylock","Loodymage","Loodypala","Loodypriest","Loodystab","Loodywar","Mcgorlock","Mcoprah","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Skillisue","Xencowmoo","Xentasia","Xentasiaa","Xën"} end F()
+F = function() provider.db["MirageRaceway"]={100,"Anurios","Aquaxxus","Astolffo","Barahunts","Baralia","Baraline","Baráth","Braldruid","Brathra","Brusekabine","Chaosjones","Deladin","Delfury","Delhancement","Delshank","Drevani","Dýnem","Estouls","Faeden","Kalepp","Kyrielight","Legendfusion","Loneta","Nishandra","Nyastolfo","Sanshein","Saphirar","Saphyron","Selenas","Shadylinkz","Taala","Tamamonomae","Teranosh","Theddios","Theddious","Thispala","Vezstrike","Whichpala","Whopala","Zamaas"} end F()
+F = function() provider.db["Auberdine"]={180,"Antistäminik","Lunou"} end F()
+F = function() provider.db["Sulfuron"]={184,"Manakan","Taigâ"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-05-22T06:09:36Z",numCharacters=124,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2025-05-23T06:09:27Z",numCharacters=122,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 58
-F = function() provider.lookup[1] = "\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\4\4\4\4" end F()
+-- chunk size: 56
+F = function() provider.lookup[1] = "\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\10\5\4\4\4\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
