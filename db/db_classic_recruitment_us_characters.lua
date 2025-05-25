472439dcfcbfea153990024a86dd30f178eadd0c
Jah Raphael <jahraphael@yahoo.com>
2025-05-25 06:11:20 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-05-24T06:09:54Z",numCharacters=128,db={}}
+local provider={name=...,data=3,region="us",date="2025-05-25T06:09:59Z",numCharacters=118,db={}}
 local F
 
 F = function() provider.db["Mankrik"]={0,"Asaga","Banksun","Cholerik","Dinkdang","Evilsnowman","Fatheroboney","Funadin","Fundeath","Grimnim","Gunsun","Hentaicles","Holysun","Kawaichi","Lisunn","Locksun","Longfínger","Phyrexiah","Polihoes","Randompain","Shoeash","Suncane","Sunded","Sungone","Sunnymachine","Sunsham","Sunshift","Tiqqletotem","Tøuka","Zetsuichi"} end F()
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-05-24T06:09:54Z",numCharacters=128,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2025-05-25T06:09:59Z",numCharacters=118,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 62
