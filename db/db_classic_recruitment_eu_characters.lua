139f1cefc7a8f9d7dc5ea5ad719495fea24c0e58
Jah Raphael <jahraphael@yahoo.com>
2025-05-26 06:13:27 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,13 +2,13 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-05-25T06:09:59Z",numCharacters=118,db={}}
+local provider={name=...,data=3,region="eu",date="2025-05-26T06:12:57Z",numCharacters=113,db={}}
 local F
 
 F = function() provider.db["Gehennas"]={0,"Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Goblinpung","Grandpá","Hacezbank","Holymoleh","Juben","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Loodycat","Loodydk","Loodyhunt","Loodylock","Loodymage","Loodypala","Loodypriest","Loodystab","Loodywar","Mcgorlock","Mcoprah","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Skillisue","Xencowmoo","Xentasia","Xën"} end F()
-F = function() provider.db["Auberdine"]={98,"Antistäminik","Lunou"} end F()
-F = function() provider.db["MirageRaceway"]={102,"Anurios","Aquaxxus","Astolffo","Barahunts","Baralia","Baraline","Baráth","Braldruid","Brathra","Brusekabine","Chaosjones","Deladin","Delfury","Delhancement","Delshank","Drevani","Estouls","Kalepp","Kyrielight","Lethtendria","Mukunaru","Nishandra","Nyastolfo","Saphirar","Saphyron","Selenas","Tamamonomae","Teranosh","Theddios","Theddious","Thispala","Whichpala","Whopala","Zamaas"} end F()
-F = function() provider.db["Sulfuron"]={170,"Manakan","Taigâ"} end F()
+F = function() provider.db["MirageRaceway"]={98,"Anurios","Aquaxxus","Astolffo","Barahunts","Baralia","Baraline","Baráth","Braldruid","Brathra","Brusekabine","Chaosjones","Deladin","Delfury","Delhancement","Delshank","Drevani","Estouls","Kalepp","Kyrielight","Nishandra","Nyastolfo","Saphirar","Saphyron","Selenas","Tamamonomae","Teranosh","Theddios","Theddious","Thispala","Whichpala","Whopala","Zamaas"} end F()
+F = function() provider.db["Sulfuron"]={162,"Manakan","Taigâ"} end F()
+F = function() provider.db["Everlook"]={166,"Geeny","Nexxies"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
