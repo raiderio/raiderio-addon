ee37fb1c99a7c1f621127ec754ae52531d48a69a
Jah Raphael <jahraphael@yahoo.com>
2025-06-18 06:13:17 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,16 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-06-17T06:13:39Z",numCharacters=179,db={}}
+local provider={name=...,data=3,region="eu",date="2025-06-18T06:12:19Z",numCharacters=184,db={}}
 local F
 
 F = function() provider.db["Golemagg"]={0,"Caismeow","Ien","Pilavjr","Qmidruid","Strungôl"} end F()
-F = function() provider.db["Gehennas"]={10,"Avaich","Erycedk","Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Greyclap","Hacezbank","Holymoleh","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Lynary","Mcgorlock","Mcoprah","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Shyyz"} end F()
-F = function() provider.db["PyrewoodVillage"]={86,"Blodynysfa","Eatmeguldan","Eatmeorihime","Mojonicus"} end F()
-F = function() provider.db["Firemaw"]={94,"Daoski","Daowy"} end F()
-F = function() provider.db["MirageRaceway"]={98,"Anurios","Aquaxxus","Astolffo","Barahunts","Baralia","Baraline","Baráth","Braldruid","Brathra","Brusekabine","Chaosjones","Deladin","Delfury","Delhancement","Delightfur","Delshank","Drevani","Estouls","Flowersnores","Kalepp","Kyrielight","Lethtendria","Mukunaru","Nishandra","Nyastolfo","Saphirar","Saphyron","Selenas","Tamamonomae","Teranosh","Theddios","Theddious","Thispala","Whichpala","Whopala","Zamaas"} end F()
-F = function() provider.db["Sulfuron"]={170,"Manakan","Taigâ"} end F()
-F = function() provider.db["Everlook"]={174,"Agarnith","Ghortus","Ghôrtûs","Hîmmbärlie","Jessýca","Tabaja","Yîngs"} end F()
+F = function() provider.db["Gehennas"]={10,"Avaich","Erycedk","Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Greyclap","Hacezbank","Holymoleh","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Lynary","Mcgorlock","Mcoprah","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress"} end F()
+F = function() provider.db["PyrewoodVillage"]={84,"Blodynysfa","Eatmeguldan","Eatmeorihime","Mojonicus"} end F()
+F = function() provider.db["Firemaw"]={92,"Daoski","Daowy"} end F()
+F = function() provider.db["MirageRaceway"]={96,"Anurios","Aquaxxus","Astolffo","Barahunts","Baralia","Baraline","Baráth","Braldruid","Brathra","Brusekabine","Chaosjones","Deladin","Delfury","Delhancement","Delightfur","Delshank","Drevani","Estouls","Flowersnores","Kalepp","Kyrielight","Lethtendria","Mukunaru","Nishandra","Nyastolfo","Saphirar","Saphyron","Selenas","Tamamonomae","Teranosh","Theddios","Theddious","Thispala","Whichpala","Whopala","Zamaas"} end F()
+F = function() provider.db["Sulfuron"]={168,"Manakan","Taigâ"} end F()
+F = function() provider.db["Everlook"]={172,"Agarnith","Ghortus","Ghôrtûs","Hîmmbärlie","Jessýca","Tabaja","Têddyy","Yîngs"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,14 +2,14 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-06-17T06:13:39Z",numCharacters=179,db={}}
+local provider={name=...,data=3,region="us",date="2025-06-18T06:12:19Z",numCharacters=184,db={}}
 local F
 
-F = function() provider.db["Faerlina"]={0,"Aquariosz","Chicoscience","Daharan","Finot","Guigo","Jaofindel","Mgbird","Mgrips","Mgsmiter","Pitufinna","Raigl","Raigs","Rckzug"} end F()
-F = function() provider.db["Pagle"]={26,"Aliceinvoid","Gertyhunt","Gertypal","Ratatatata","Shayndemic","Shaynectify","Shaynectuary","Shayneman","Shaynquility","Vyndria"} end F()
-F = function() provider.db["Mankrik"]={46,"Asaga","Bankaichi","Banksun","Cholerik","Darthlasy","Deadvon","Dinkdang","Dromlock","Droodkage","Drömmekage","Drømmekage","Evilsnowman","Fatheroboney","Forseste","Funadin","Funbear","Fundeath","Funhunt","Funwar","Fún","Grimnim","Gunsun","Hamsterw","Hamstr","Hentaicles","Holysun","Hëks","Kalegon","Kawaichi","Lasyheals","Lazylennis","Liak","Liakh","Liakp","Lisunn","Locksun","Moo","Nicktendoh","Phyrexiah","Polihoes","Randompain","Revirez","Riaieri","Shinkage","Shinkdk","Shoeash","Styggmz","Suncane","Sunded","Sungone","Sunnymachine","Sunsham","Sunshift","Sunslam","Tiqqletotem","Tolvik","Tsoklos","Tøuka","Warpock","Zetsuichi"} end F()
-F = function() provider.db["Benediction"]={166,"Chookieqtx"} end F()
-F = function() provider.db["Grobbulus"]={168,"Scampaar"} end F()
+F = function() provider.db["Faerlina"]={0,"Aquariosz","Chicoscience","Daharan","Finot","Gringles","Gringlesdk","Gringlesprst","Guigo","Jaofindel","Mgbird","Mgrips","Mgsmiter","Pitufinna","Raigl","Raigs","Rckzug"} end F()
+F = function() provider.db["Pagle"]={32,"Aliceinvoid","Gertydk","Gertyhunt","Gertym","Gertyp","Gertypal","Gertyw","Ratatatata","Shayndemic","Shaynectify","Shaynectuary","Shayneman","Shaynquility","Vyndria"} end F()
+F = function() provider.db["Mankrik"]={60,"Asaga","Bankaichi","Banksun","Cholerik","Darthlasy","Deadvon","Dinkdang","Dromlock","Droodkage","Drömmekage","Drømmekage","Evilsnowman","Fatheroboney","Funadin","Funbear","Fundeath","Funhunt","Funwar","Fún","Gobbigo","Grimnim","Gunsun","Hamsterw","Hamstr","Hentaicles","Holysun","Hëks","Kalegon","Kawaichi","Lasyheals","Lazylennis","Liak","Liakh","Lisunn","Locksun","Moo","Nicktendoh","Phyrexiah","Polihoes","Randompain","Revirez","Riaieri","Shinkage","Shinkdk","Shoeash","Styggmz","Suncane","Sunded","Sungone","Sunnymachine","Sunsham","Sunshift","Sunslam","Telonusw","Tiqqletotem","Tøuka","Warpock","Zetsuichi"} end F()
+F = function() provider.db["Benediction"]={176,"Chookieqtx"} end F()
+F = function() provider.db["Grobbulus"]={178,"Scampaar"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
