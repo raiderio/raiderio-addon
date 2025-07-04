31fb98e50d1422a581ff34bf64ed2580b759e167
Jah Raphael <jahraphael@yahoo.com>
2025-07-04 06:09:57 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,16 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-07-03T06:08:57Z",numCharacters=94,db={}}
+local provider={name=...,data=3,region="eu",date="2025-07-04T06:09:02Z",numCharacters=99,db={}}
 local F
 
-F = function() provider.db["Golemagg"]={0,"Baraka","Caismeow","Ien","Noctüa","Nöctua","Pilavjr","Strungôl"} end F()
-F = function() provider.db["Gehennas"]={14,"Avaich","Erycedk","Holymoleh","Kamarile","Lokkdk","Mcgorlock","Mcoprah","Ouiz","Stinkybo","Tabrion","Waxymoo","Wightt"} end F()
-F = function() provider.db["Lakeshire"]={38,"Fuddn","Widjuga"} end F()
-F = function() provider.db["Firemaw"]={42,"Bayzu","Daoski","Daowy","Munksen","Renser","Sitkazero","Unghertøg","Ungwar"} end F()
-F = function() provider.db["PyrewoodVillage"]={58,"Blodynysfa","Hellzie","Mojonicus"} end F()
-F = function() provider.db["Auberdine"]={64,"Etsutsu","Genësis","Njàll","Sòrrøw"} end F()
-F = function() provider.db["Sulfuron"]={72,"Manakan","Taigâ"} end F()
+F = function() provider.db["Golemagg"]={0,"Baraka","Caismeow","Ien","Noctüa","Nöctua","Pilavjr","Qmidruid","Strungôl"} end F()
+F = function() provider.db["Gehennas"]={16,"Avaich","Erycedk","Holymoleh","Kamarile","Lokkdk","Mcgorlock","Mcoprah","Ouiz","Purplhunter","Stinkybo","Tabrion","Waxymoo","Wightt"} end F()
+F = function() provider.db["Lakeshire"]={42,"Fuddn","Widjuga"} end F()
+F = function() provider.db["Firemaw"]={46,"Bayzu","Daoski","Daowy","Munksen","Renser","Sandroni","Unghertøg","Ungwar"} end F()
+F = function() provider.db["PyrewoodVillage"]={62,"Blodynysfa","Hellzie","Mojonicus"} end F()
+F = function() provider.db["Auberdine"]={68,"Etsutsu","Genësis","Njàll","Sòrrøw"} end F()
+F = function() provider.db["Sulfuron"]={76,"Manakan","Taigâ"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,13 +2,13 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-07-03T06:08:57Z",numCharacters=94,db={}}
+local provider={name=...,data=3,region="us",date="2025-07-04T06:09:02Z",numCharacters=99,db={}}
 local F
 
-F = function() provider.db["Faerlina"]={0,"Aquariosz","Chicoscience","Daharan","Dalalan","Fenyo","Finot","Guigo","Jaofindel","Mgbird","Mgrips","Mgsmiter","Mirieth","Ojdkay","Ojdmooracle","Pitufinna","Raigl","Raigs","Rckzug"} end F()
-F = function() provider.db["Mankrik"]={36,"Analyzefacts","Anoxyn","Darthlasy","Deadvon","Dromlock","Droodkage","Drömmekage","Drømmekage","Forseste","Hamsterw","Hamstr","Hëks","Kalegon","Lasyheals","Lazylennis","Liak","Liakd","Liakdk","Liakh","Liakp","Mercymohrn","Moo","Revirez","Riaieri","Rodabug","Shinkage","Shinkdk","Styggmz","Tolvik","Tsoklos","Veloth","Warpock"} end F()
-F = function() provider.db["Benediction"]={100,"Dolbyatmos","Hannahxoxo","Laeza","Mersina","Tdjakez"} end F()
-F = function() provider.db["Atiesh"]={110,"Acaylee"} end F()
+F = function() provider.db["Faerlina"]={0,"Aquariosz","Chicoscience","Daharan","Dalalan","Fenyo","Finot","Guigo","Jaofindel","Mgbird","Mgrips","Mgsmiter","Ojdkay","Ojdmooracle","Pitufinna","Raigl","Raigs","Rckzug"} end F()
+F = function() provider.db["Mankrik"]={34,"Analyzefacts","Anoxyn","Darthlasy","Deadvon","Dromlock","Droodkage","Drömmekage","Drømmekage","Gobbigo","Gralmokt","Hamsterw","Hamstr","Hëks","Kalegon","Lasyheals","Lazylennis","Liak","Liakd","Liakdk","Liakh","Liakk","Mercymohrn","Moggums","Moo","Revirez","Riaieri","Rodabug","Shinkage","Shinkdk","Styggmz","Telonusw","Thormclorg","Veloth","Warpock"} end F()
+F = function() provider.db["Benediction"]={102,"Dolbyatmos","Hannahxoxo","Khroghorn","Laeza","Mersina","Raezza","Tdjakez"} end F()
+F = function() provider.db["Atiesh"]={116,"Acaylee"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
