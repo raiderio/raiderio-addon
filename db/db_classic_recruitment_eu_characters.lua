37f13ac86239dae4a7731a078c1b99f34b9b60eb
Jah Raphael <jahraphael@yahoo.com>
2025-07-16 06:12:43 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-07-15T06:09:45Z",numCharacters=121,db={}}
+local provider={name=...,data=3,region="eu",date="2025-07-16T06:12:25Z",numCharacters=111,db={}}
 local F
 
-F = function() provider.db["Golemagg"]={0,"Baraka","Caismeow","Ien","Lissà","Noctüa","Nöctua","Pilavjr","Qmidruid","Samanamana","Strungôl"} end F()
-F = function() provider.db["Gehennas"]={20,"Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Hacezbank","Holymoleh","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Lokkdk","Mcgorlock","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Stinkybo","Waxymoo","Wightt"} end F()
+F = function() provider.db["Golemagg"]={0,"Baraka","Caismeow","Ien","Noctüa","Nöctua","Pilavjr","Qmidruid","Samanamana","Strungôl"} end F()
+F = function() provider.db["Gehennas"]={18,"Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exendri","Exmeralda","Hacezbank","Hresources","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Lokkdk","Mcfu","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Progress","Purplhunter","Stinkybo","Waxymoo","Wightt"} end F()
 F = function() provider.db["Lakeshire"]={92,"Fuddn","Guterpalaxy","Palazondes","Waffenwanda","Widjuga"} end F()
 F = function() provider.db["Firemaw"]={102,"Munksen","Mænix","Unghertøg","Ungwar"} end F()
 F = function() provider.db["Auberdine"]={110,"Etsutsu","Etsutsuu","Etsuu","Genësis","Njàll"} end F()
