97b03158f178e5040afdd5d3c07d9c384a0e4280
Jah Raphael <jahraphael@yahoo.com>
2025-08-20 06:03:50 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,21 +2,20 @@
 -- Copyright (c) 2025 by Ludicrous Speed, LLC
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-08-19T06:02:48Z",numCharacters=306,db={}}
+local provider={name=...,data=3,region="eu",date="2025-08-20T06:03:04Z",numCharacters=300,db={}}
 local F
 
-F = function() provider.db["Gehennas"]={0,"Bgmagemitko","Bogosort","Brewstar","Cantiez","Canty","Chimit","Crankilockin","Crankipanda","Cubê","Devilmonk","Devilsham","Devilshamtre","Ehldur","Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exmeralda","Fartbozz","Flednogg","Fretful","Granie","Hacezbank","Healors","Hresources","Idiotic","Ireful","Jesiper","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Kimdecay","Kimdruid","Kimfist","Komithunt","Lightmit","Lildikgang","Livid","Lokkmage","Londonderry","Masuke","Mcfu","Mitcow","Monstrbrakam","Nemehealvai","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Ohhkiakiakia","Progress","Purpr","Scarpacashew","Scarpadk","Shaili","Slicenrice","Soloinc","Starkmag","Sulfius","Terrymunro","Tovalina","Whistle","Wikeed","Zepheron"} end F()
-F = function() provider.db["Golemagg"]={158,"Amrael","Celestira","Deathecho","Gloyier","Jsefigueiras","Nightish","Tallyion"} end F()
-F = function() provider.db["Sulfuron"]={172,"Altride","Asphar","Asphyxi","Ennö","Jppjtank","Sady","Shankün","Slackeur","Souffle"} end F()
-F = function() provider.db["Lakeshire"]={190,"Fuddn","Widjuga"} end F()
-F = function() provider.db["Firemaw"]={194,"Airyu","Bayzu","Berserkly","Bullgray","Daoski","Daowy","Daozu","Encorey","Eryu","Klëmens","Lanadelfaith","Lurké","Madisondeer","Minidíva","Monksand","Munksen","Patthiasm","Razzp","Sandroni","Serfilas","Shadowandy","Tholo","Unghertøg","Ungwar","Énérÿ","Énërgy","Ênêrgÿ","Ênërgy","Ënergy","Ënërgy"} end F()
-F = function() provider.db["MirageRaceway"]={254,"Ashwarlock","Bråkis","Crabdk","Elbatso","Hyldster","Jayiope","Ldotb","Lotararise","Ragnarwowbro","Scalpels","Skengah","Slomopa","Valdomir"} end F()
-F = function() provider.db["PyrewoodVillage"]={280,"Duarni","Kyoodeath"} end F()
-F = function() provider.db["Everlook"]={284,"Cnyder","Cnydèr","Fishbones","Friedabella","Gugugagi","Jazzlynn","Miamelinaqt","Phyrrón"} end F()
-F = function() provider.db["Venoxis"]={300,"Dahlili","Sherroxx"} end F()
-F = function() provider.db["Mandokir"]={304,"Bombigl","Fufi","Fufigl","Goldfinia"} end F()
-F = function() provider.db["Auberdine"]={312,"Orná"} end F()
-F = function() provider.db["Patchwerk"]={314,"Detrimental"} end F()
+F = function() provider.db["Gehennas"]={0,"Bgmagemitko","Bogosort","Brewstar","Cantiez","Canty","Chimit","Crankilockin","Crankipanda","Cubê","Devilmonk","Devilsham","Devilshamtre","Ehldur","Exandero","Exanderr","Exandk","Exandri","Exandria","Exandro","Exandrova","Exandru","Exmeralda","Fartbozz","Flednogg","Fretful","Granie","Hacezbank","Healors","Hresources","Idiotic","Ireful","Jesiper","Juliusb","Juliusm","Juliusp","Juliust","Juliusw","Juliuswlx","Juliuswx","Juliusxp","Juliusxpx","Kamarile","Kimdecay","Kimdruid","Kimfist","Komithunt","Lightmit","Lildikgang","Livid","Lokkmage","Londonderry","Masuke","Mcfu","Mitcow","Monstrbrakam","Nemehealvai","Occbull","Occdk","Occdkxy","Occdruid","Occdruidx","Occdruidxy","Occmage","Occmagex","Ohhkiakiakia","Pleasurefist","Progress","Purpr","Scarpacashew","Scarpadk","Shaili","Slicenrice","Soloinc","Starkmag","Stelfí","Sulfius","Terrymunro","Tovalina","Whistle","Wikeed","Wocyxes","Zepheron"} end F()
+F = function() provider.db["Golemagg"]={164,"Amrael","Celestira","Deathecho","Gloyier","Jsefigueiras","Justfrosty","Justfrostydr","Justfrostym","Nightish","Tallyion"} end F()
+F = function() provider.db["Sulfuron"]={184,"Altride","Asphar","Asphyxi","Ennö","Jppjtank","Sady","Shankün","Slackeur","Souffle","Äzaghäl"} end F()
+F = function() provider.db["Lakeshire"]={204,"Fuddn","Widjuga"} end F()
+F = function() provider.db["Firemaw"]={208,"Airyu","Bayzu","Berserkly","Bullgray","Daoski","Daowy","Daozu","Encorey","Eryu","Klëmens","Lanadelfaith","Lurké","Madisondeer","Minidíva","Monksand","Munksen","Patthiasm","Razzp","Sandroni","Tholo","Unghertøg","Ungwar","Énërgy","Ênêrgÿ","Ênërgy","Ënergy","Ënërgy"} end F()
+F = function() provider.db["MirageRaceway"]={262,"Arianeus","Ashwarlock","Batsy","Bråkis","Crabdk","Deerf","Elbatso","Froztier","Jayiope","Ldotb","Lotararise","Ragnarwowbro","Scalpels","Skengah","Valdomir"} end F()
+F = function() provider.db["PyrewoodVillage"]={292,"Kyoodeath"} end F()
+F = function() provider.db["Everlook"]={294,"Cnyder","Cnydèr","Fishbones","Friedabella","Gugugagi","Jazzlynn","Miamelinaqt","Phyrrón"} end F()
+F = function() provider.db["Venoxis"]={310,"Dahlili","Sherroxx"} end F()
+F = function() provider.db["Mandokir"]={314,"Bombigl","Fufi","Fufigl","Goldfinia"} end F()
+F = function() provider.db["Patchwerk"]={322,"Detrimental"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
