deb568de73edd310a165b71f22db88f13efe647c
Jah Raphael <jahraphael@yahoo.com>
2025-09-19 08:43:20 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2025-09-18T08:22:06Z",numCharacters=35487,db={}}
+local provider={name=...,data=3,region="kr",date="2025-09-19T08:33:40Z",numCharacters=36085,db={}}
 local F
 
 F = function() provider.db["헬스크림"]={0,"홍타우","홍탁기","후신타후"} end F()
--- db/db_recruitment_kr_lookup.lua
+++ db/db_recruitment_kr_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2025-09-18T08:22:06Z",numCharacters=35487,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="kr",date="2025-09-19T08:33:40Z",numCharacters=36085,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 6
