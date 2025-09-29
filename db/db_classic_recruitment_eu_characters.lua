5dc67669e9d3fab1ebc39d8034486267622992e3
Jah Raphael <jahraphael@yahoo.com>
2025-09-29 06:05:48 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,13 +2,13 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-09-28T06:04:57Z",numCharacters=96,db={}}
+local provider={name=...,data=3,region="eu",date="2025-09-29T06:04:53Z",numCharacters=96,db={}}
 local F
 
 F = function() provider.db["PyrewoodVillage"]={0,"Keavarie","Mortalitus"} end F()
-F = function() provider.db["Gehennas"]={4,"Ebonnie","Ehldur","Hekseren","Helisha","Hresources","Kamarile","Kéthaze","Mcgorlock","Nachoj","Nayaraw","Orcoholic","Seythez","Seythezqt","Seythezz","Stelfí","Uroyamï","Åb"} end F()
-F = function() provider.db["MirageRaceway"]={38,"Eizon","Fehica","Rawrhehe"} end F()
-F = function() provider.db["Auberdine"]={44,"Ayliea","Gaweed","Køølgoth","Sílver","Thenaryon"} end F()
+F = function() provider.db["Gehennas"]={4,"Ebonnie","Ehldur","Helisha","Hresources","Kamarile","Kéthaze","Mcgorlock","Nachoj","Nayaraw","Orcoholic","Seythez","Seythezqt","Seythezz","Stelfí","Uroyamï","Åb"} end F()
+F = function() provider.db["MirageRaceway"]={36,"Eizon","Fehica","Rawrhehe"} end F()
+F = function() provider.db["Auberdine"]={42,"Ayliea","Gaweed","Køølgoth","Sílver","Thenaryon","Tømo"} end F()
 F = function() provider.db["Firemaw"]={54,"Fiftyfiva","Grohaila","Lorand","Lorandof","Lorandoh","Lorandó","Neferur","Schwesters","Songss","Swiftrock","Zpinger"} end F()
 F = function() provider.db["Flamegor"]={76,"Стэсиша"} end F()
 
