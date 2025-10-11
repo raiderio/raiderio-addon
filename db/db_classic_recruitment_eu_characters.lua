b2085b5fa10529999e62866b6a70f8b81f008f35
Jah Raphael <jahraphael@yahoo.com>
2025-10-11 06:06:39 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,15 +2,16 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-10-10T06:05:03Z",numCharacters=47,db={}}
+local provider={name=...,data=3,region="eu",date="2025-10-11T06:05:41Z",numCharacters=49,db={}}
 local F
 
 F = function() provider.db["PyrewoodVillage"]={0,"Keavarie"} end F()
 F = function() provider.db["Auberdine"]={2,"Agrou","Amelyss","Ayliea","Beurbz","Birbichette","Gaweed","Køølgoth","Lomyah","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Sílver","Thenaryon","Wabe","Ïllythras"} end F()
 F = function() provider.db["MirageRaceway"]={36,"Arooq","Eizon","Fehica","Rawrhehe"} end F()
-F = function() provider.db["Gehennas"]={44,"Helisha","Nachoj","Åb"} end F()
-F = function() provider.db["Firemaw"]={50,"Fiftyfiva"} end F()
-F = function() provider.db["Everlook"]={52,"Michaa"} end F()
+F = function() provider.db["Gehennas"]={44,"Nachoj"} end F()
+F = function() provider.db["Garalon"]={46,"Hresources","Jonmena","Kamarile","Mcfat"} end F()
+F = function() provider.db["Firemaw"]={54,"Fiftyfiva"} end F()
+F = function() provider.db["Shek'zeer"]={56,"Ashvyra"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-10-10T06:05:03Z",numCharacters=47,db={}}
+local provider={name=...,data=3,region="us",date="2025-10-11T06:05:41Z",numCharacters=49,db={}}
 local F
 
 F = function() provider.db["Pagle"]={0,"Amalthae","Blinkinblake","Bounsay","Eirié","Emerlaus","Finatae","Gathormackoy","Holysesher","Lessholy","Namereported","Nanajan","Slashme","Touchofkarma","Uno","Wethealussy","Whozewhatzit","Xatrina","Xdxdxdxdxdxd","Álejandro","Álexander"} end F()
