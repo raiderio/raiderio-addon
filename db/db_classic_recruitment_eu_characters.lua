c2e6744e98a06cb7144ffe9d41316130b29d53ea
Jah Raphael <jahraphael@yahoo.com>
2025-10-13 06:06:44 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,15 +2,13 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-10-12T06:05:48Z",numCharacters=51,db={}}
+local provider={name=...,data=3,region="eu",date="2025-10-13T06:05:52Z",numCharacters=49,db={}}
 local F
 
-F = function() provider.db["PyrewoodVillage"]={0,"Keavarie"} end F()
-F = function() provider.db["Auberdine"]={2,"Agrou","Amelyss","Ayliea","Birbichette","Køølgoth","Lomyah","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Sílver","Thenaryon","Tømo","Ïllythras"} end F()
-F = function() provider.db["MirageRaceway"]={32,"Arooq","Eizon","Fehica","Rawrhehe"} end F()
-F = function() provider.db["Gehennas"]={40,"Nachoj"} end F()
-F = function() provider.db["Garalon"]={42,"Hresources","Jonmena","Kamarile","Mcfat"} end F()
-F = function() provider.db["Shek'zeer"]={50,"Ashvyra","Fyrnokk"} end F()
+F = function() provider.db["Auberdine"]={0,"Agrou","Amelyss","Ayliea","Birbichette","Køølgoth","Lomyah","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Sílver","Thenaryon","Tømo","Ïllythras"} end F()
+F = function() provider.db["MirageRaceway"]={30,"Arooq","Eizon","Fehica","Rawrhehe"} end F()
+F = function() provider.db["Gehennas"]={38,"Nachoj"} end F()
+F = function() provider.db["Garalon"]={40,"Hresources","Jonmena","Kamarile","Mcfat"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,10 +2,10 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-10-12T06:05:48Z",numCharacters=51,db={}}
+local provider={name=...,data=3,region="us",date="2025-10-13T06:05:52Z",numCharacters=49,db={}}
 local F
 
-F = function() provider.db["Pagle"]={0,"Amalthae","Anaranjado","Bounsay","Calsharpsdk","Calsharpton","Cougarstoves","Eirié","Emerlaus","Finatae","Gathormackoy","Holysesher","Immerie","Lessholy","Namereported","Nanajan","Slashme","Touchofkarma","Uno","Wethealussy","Whozewhatzit","Xatrina","Xdxdxdxdxdxd","Álejandro","Álexander"} end F()
+F = function() provider.db["Pagle"]={0,"Amalthae","Anaranjado","Blinkinblake","Bounsay","Calsharpsdk","Calsharpton","Cougarstoves","Eirié","Emerlaus","Finatae","Gathormackoy","Holysesher","Immerie","Lessholy","Namereported","Nanajan","Slashme","Touchofkarma","Uno","Wethealussy","Whozewhatzit","Xatrina","Xdxdxdxdxdxd","Álejandro","Álexander"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
