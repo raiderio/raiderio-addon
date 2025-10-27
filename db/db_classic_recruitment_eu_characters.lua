e49e534a3a35f2e2ad8ea3ef3e6b750f47d03670
Jah Raphael <jahraphael@yahoo.com>
2025-10-27 06:06:39 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,13 +2,13 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-10-26T06:06:21Z",numCharacters=41,db={}}
+local provider={name=...,data=3,region="eu",date="2025-10-27T06:06:25Z",numCharacters=47,db={}}
 local F
 
 F = function() provider.db["Auberdine"]={0,"Agrou","Amelyss","Arrowwing","Baketto","Birbichette","Chaukpyk","Dirtyoldman","Koukyphonden","Lomyah","Meyliä","Miazar","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Shaukobydon","Warrog","Ïllythras"} end F()
-F = function() provider.db["Hoptallus"]={38,"Arizara","Ludidoktor","Nikotako","Psyvise","Rualan"} end F()
-F = function() provider.db["Garalon"]={48,"Blackmamba","Disnec","Gaddar","Hresources","Inyâ","Jonmena","Kamarile","Mcfat","Nayaraw","Nyxbane"} end F()
-F = function() provider.db["Norushen"]={68,"Seruss","Tonzil"} end F()
+F = function() provider.db["Hoptallus"]={38,"Arizara","Ludidoktor","Ludidoktorr","Ludidokttor","Nacugivara","Nikotako","Noshecant","Psyvise","Rualan"} end F()
+F = function() provider.db["Garalon"]={56,"Blackmamba","Disnec","Gaddar","Hresources","Inyâ","Jonmena","Kamarile","Mcfat","Nayaraw","Nyxbane"} end F()
+F = function() provider.db["Norushen"]={76,"Seruss","Tonzil"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-10-26T06:06:21Z",numCharacters=41,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2025-10-27T06:06:25Z",numCharacters=47,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 72
-F = function() provider.lookup[1] = "\10\5\10\5\10\13\10\13\10\5\10\13\10\13\10\13\10\5\10\13\10\13\10\5\10\5\10\5\10\5\10\5\10\13\10\13\10\5\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\13\10\29\10\13\10\13\10\13\4\4\10\29\10\5\10\5" end F()
+-- chunk size: 80
+F = function() provider.lookup[1] = "\10\5\10\5\10\13\10\13\10\5\10\13\10\13\10\13\10\5\10\13\10\13\10\5\10\5\10\5\10\5\10\5\10\13\10\13\10\5\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\29\10\13\10\29\10\13\10\13\10\13\4\4\10\29\10\5\10\5" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
