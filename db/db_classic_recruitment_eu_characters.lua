6e7903b57e7052451c87579a0371918c8f9be9bd
Jah Raphael <jahraphael@yahoo.com>
2025-10-29 06:07:15 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,14 +2,14 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-10-28T06:06:09Z",numCharacters=49,db={}}
+local provider={name=...,data=3,region="eu",date="2025-10-29T06:06:31Z",numCharacters=53,db={}}
 local F
 
-F = function() provider.db["Auberdine"]={0,"Agrou","Amelyss","Arrowwing","Baketto","Beurbz","Birbichette","Chaukpyk","Dirtyoldman","Koukyphonden","Lomyah","Meyliä","Miazar","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Shaukobydon","Wabe","Warrog","Ïllythras"} end F()
-F = function() provider.db["Giantstalker"]={42,"Anaralia","Baldnutsack"} end F()
-F = function() provider.db["Hoptallus"]={46,"Arizara","Ludidoktor","Ludidoktorr","Ludidokttor","Nacugivara","Nikotako","Noshecant","Psyvise","Rualan"} end F()
-F = function() provider.db["Garalon"]={64,"Blackmamba","Disnec","Gaddar","Hresources","Inyâ","Jonmena","Kamarile","Mcfat","Nayaraw","Nyxbane"} end F()
-F = function() provider.db["Norushen"]={84,"Seruss","Tonzil"} end F()
+F = function() provider.db["Auberdine"]={0,"Agrou","Amelyss","Arrowwing","Baketto","Birbichette","Chaukpyk","Dirtyoldman","Koukyphonden","Lomyah","Meyliä","Miazar","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Shaukobydon","Warrog","Ïllythras"} end F()
+F = function() provider.db["Giantstalker"]={38,"Anaralia","Baldnutsack"} end F()
+F = function() provider.db["Garalon"]={42,"Blackmamba","Disnec","Drinar","Feytsham","Hrdismissed","Hresources","Hårdknut","Inyâ","Jonmena","Kaliber","Kamarile","Kamerile","Llurk","Mcfat","Nayaraw","Nyxbane","Senicyl","Tubbywubby"} end F()
+F = function() provider.db["Hoptallus"]={78,"Arizara","Ludidoktor","Nikotako","Psyvise","Rualan"} end F()
+F = function() provider.db["Norushen"]={88,"Seruss","Tonzil"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
