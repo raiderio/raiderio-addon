3217c0e4ff22318b7d549d809493ce74bf508fb2
Jah Raphael <jahraphael@yahoo.com>
2025-11-10 06:08:39 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,17 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-11-09T06:07:49Z",numCharacters=67,db={}}
+local provider={name=...,data=3,region="eu",date="2025-11-10T06:08:00Z",numCharacters=76,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Dýnem","Gordore","Legendfusion","Loneta","Sanshein","Shadylínks","Taala","Vezstrike","Vincennes"} end F()
-F = function() provider.db["Auberdine"]={18,"Agrou","Amelyss","Arrowwing","Baketto","Beurbz","Birbichette","Dirtyoldman","Koukyphonden","Lomyah","Meyliä","Meymonk","Miazar","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Shaukobydon","Wabe","Warrog","Ïllythras"} end F()
-F = function() provider.db["Giantstalker"]={60,"Anaralia","Baldnutsack"} end F()
-F = function() provider.db["Garalon"]={64,"Blackmamba","Disnec","Drinar","Feytsham","Hrdismissed","Hresources","Hårdknut","Inyâ","Jonmena","Kaliber","Kamerile","Llurk","Mcfat","Nayaraw","Nyxbane","Senicyl","Tubbywubby"} end F()
-F = function() provider.db["Hoptallus"]={98,"Arizara","Ludidoktor","Nikotako","Psyvise","Rualan"} end F()
-F = function() provider.db["Norushen"]={108,"Lazyrage","Seruss","Tonzil"} end F()
-F = function() provider.db["Flamegor"]={114,"Синастрия","Тэтцуо"} end F()
+F = function() provider.db["Everlook"]={18,"Bélga","Xìania","Ânyrogue"} end F()
+F = function() provider.db["Auberdine"]={24,"Agrou","Amelyss","Arrowwing","Baketto","Beurbz","Birbichette","Dirtyoldman","Koukyphonden","Lomyah","Meyliä","Meymonk","Miazar","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Shaukobydon","Wabe","Warrog","Ïllythras"} end F()
+F = function() provider.db["Giantstalker"]={66,"Anaralia","Baldnutsack"} end F()
+F = function() provider.db["Garalon"]={70,"Blackmamba","Disnec","Drinar","Feytsham","Hrdismissed","Hresources","Hårdknut","Inyâ","Jonmena","Kaliber","Kamerile","Llurk","Mcfat","Nayaraw","Nyxbane","Senicyl","Tubbywubby"} end F()
+F = function() provider.db["Hoptallus"]={104,"Arizara","Ludidoktor","Ludidoktorr","Ludidokttor","Nacugivara","Nikotako","Noshecant","Psyvise","Rualan"} end F()
+F = function() provider.db["Norushen"]={122,"Lazyrage","Seruss","Tonzil"} end F()
+F = function() provider.db["Flamegor"]={128,"Синастрия","Тэтцуо"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-11-09T06:07:49Z",numCharacters=67,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2025-11-10T06:08:00Z",numCharacters=76,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 16
-F = function() provider.lookup[1] = "\10\29\10\13\10\29\10\29\10\29\10\13\10\13\10\29" end F()
+-- chunk size: 20
+F = function() provider.lookup[1] = "\10\29\10\13\10\29\10\29\10\13\10\13\10\29\10\29\10\29\10\29" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
