6514f204cbbd39ec1353156a3b552f06b3356fe9
Jah Raphael <jahraphael@yahoo.com>
2025-11-14 06:09:01 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-11-13T06:08:06Z",numCharacters=60,db={}}
+local provider={name=...,data=3,region="eu",date="2025-11-14T06:08:11Z",numCharacters=58,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Sanshein"} end F()
@@ -11,8 +11,8 @@ F = function() provider.db["Auberdine"]={18,"Agrou","Amelyss","Arrowwing","Baket
 F = function() provider.db["Giantstalker"]={56,"Anaralia","Baldnutsack"} end F()
 F = function() provider.db["Garalon"]={60,"Blackmamba","Disnec","Drinar","Feytsham","Hrdismissed","Hresources","Hårdknut","Inyâ","Jonmena","Kaliber","Kamerile","Llurk","Mcfat","Nayaraw","Nyxbane","Senicyl","Tubbywubby"} end F()
 F = function() provider.db["Hoptallus"]={94,"Arizara","Ludidoktor","Nikotako","Psyvise","Rualan"} end F()
-F = function() provider.db["Norushen"]={104,"Lazyrage","Seruss","Tonzil"} end F()
-F = function() provider.db["Flamegor"]={110,"Синастрия","Тэтцуо"} end F()
+F = function() provider.db["Norushen"]={104,"Lazyrage"} end F()
+F = function() provider.db["Flamegor"]={106,"Синастрия","Тэтцуо"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
