54152f4fcfda9246d4726b5211ce3e01923b2b9b
Jah Raphael <jahraphael@yahoo.com>
2025-11-26 06:12:10 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,16 +2,16 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-11-25T06:10:53Z",numCharacters=49,db={}}
+local provider={name=...,data=3,region="eu",date="2025-11-26T06:10:54Z",numCharacters=48,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Sanshein"} end F()
-F = function() provider.db["Everlook"]={2,"Anyruok","Anýmonk","Anýrogue","Bélga","Ii","Xìania","Ânydk","Ânyrogue"} end F()
-F = function() provider.db["Auberdine"]={18,"Agrou","Amelyss","Beurbz","Birbichette","Lihya","Lomyah","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Wabe","Ïllythras"} end F()
+F = function() provider.db["Everlook"]={2,"Anyruok","Anýmonk","Anýrogue","Anýshus","Bélga","Ii","Jasunaqtxd","Xìania","Ânydk","Ânyrogue"} end F()
+F = function() provider.db["Auberdine"]={22,"Agrou","Amelyss","Birbichette","Lihya","Lomyah","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Ïllythras"} end F()
 F = function() provider.db["Giantstalker"]={44,"Anaralia","Baldnutsack"} end F()
 F = function() provider.db["Garalon"]={48,"Aresdk","Drinar","Feytsham","Hrdismissed","Hårdknut","Kaliber","Llurk","Senicyl","Tubbywubby"} end F()
 F = function() provider.db["Hoptallus"]={66,"Arizara","Feronya","Ludidoktor","Ludidoktorr","Ludidokttor","Nacugivara","Nikotako","Noshecant","Psyvise","Rualan"} end F()
-F = function() provider.db["Flamegor"]={86,"Байрамов","Тэтцуо","Эритропоетин"} end F()
+F = function() provider.db["Flamegor"]={86,"Байрамов","Эритропоетин"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-11-25T06:10:53Z",numCharacters=49,db={}}
+local provider={name=...,data=3,region="us",date="2025-11-26T06:10:54Z",numCharacters=48,db={}}
 local F
 
 F = function() provider.db["Pagle"]={0,"Bussifistr","Daemonheart","Grizznos"} end F()
