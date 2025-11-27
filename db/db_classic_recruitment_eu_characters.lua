01bbdc4053c99bb40c5ff3331976a29335c742b0
Jah Raphael <jahraphael@yahoo.com>
2025-11-27 06:12:00 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2025-11-26T06:10:54Z",numCharacters=48,db={}}
+local provider={name=...,data=3,region="eu",date="2025-11-27T06:11:12Z",numCharacters=47,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Sanshein"} end F()
@@ -10,8 +10,8 @@ F = function() provider.db["Everlook"]={2,"Anyruok","Anýmonk","Anýrogue","Aný
 F = function() provider.db["Auberdine"]={22,"Agrou","Amelyss","Birbichette","Lihya","Lomyah","Nylâh","Pinocchio","Pylaf","Shalanore","Shaopao","Ïllythras"} end F()
 F = function() provider.db["Giantstalker"]={44,"Anaralia","Baldnutsack"} end F()
 F = function() provider.db["Garalon"]={48,"Aresdk","Drinar","Feytsham","Hrdismissed","Hårdknut","Kaliber","Llurk","Senicyl","Tubbywubby"} end F()
-F = function() provider.db["Hoptallus"]={66,"Arizara","Feronya","Ludidoktor","Ludidoktorr","Ludidokttor","Nacugivara","Nikotako","Noshecant","Psyvise","Rualan"} end F()
-F = function() provider.db["Flamegor"]={86,"Байрамов","Эритропоетин"} end F()
+F = function() provider.db["Hoptallus"]={66,"Arizara","Drbuckshot","Feronya","Ludidoktor","Nazorathi","Nikotako","Palladan","Psyvise","Rualan"} end F()
+F = function() provider.db["Flamegor"]={84,"Байрамов","Эритропоетин"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2025-11-26T06:10:54Z",numCharacters=48,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2025-11-27T06:11:12Z",numCharacters=47,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 6
