fe16df56ce1c8443ff7144acb9ce70a263a41083
Jah Raphael <jahraphael@yahoo.com>
2026-01-09 06:10:12 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,14 +2,14 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-01-08T06:09:25Z",numCharacters=15,db={}}
+local provider={name=...,data=3,region="eu",date="2026-01-09T06:09:11Z",numCharacters=19,db={}}
 local F
 
-F = function() provider.db["Hoptallus"]={0,"Arizara","Drdottington","Eruveena","Feronya","Ludidoktor","Nikotako","Palladan","Psyvise","Rualan"} end F()
-F = function() provider.db["Norushen"]={18,"Seruss","Tonzil"} end F()
-F = function() provider.db["OokOok"]={22,"Lauryen","Twitchcamgrl"} end F()
-F = function() provider.db["Garalon"]={26,"Grundo"} end F()
-F = function() provider.db["Everlook"]={28,"Onehitshock"} end F()
+F = function() provider.db["Hoptallus"]={0,"Arizara","Drdottington","Eruveena","Feronya","Ludidoktor","Ludidoktorr","Ludidokttor","Nacugivara","Nikotako","Noshecant","Palladan","Psyvise","Rualan"} end F()
+F = function() provider.db["Norushen"]={26,"Seruss","Tonzil"} end F()
+F = function() provider.db["OokOok"]={30,"Lauryen","Twitchcamgrl"} end F()
+F = function() provider.db["Garalon"]={34,"Grundo"} end F()
+F = function() provider.db["Everlook"]={36,"Onehitshock"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-01-08T06:09:25Z",numCharacters=15,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2026-01-09T06:09:11Z",numCharacters=19,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 30
-F = function() provider.lookup[1] = "\10\17\10\5\10\17\10\17\10\17\10\17\10\5\10\17\10\17\10\5\10\5\4\16\4\16\4\4\4\8" end F()
+-- chunk size: 38
+F = function() provider.lookup[1] = "\10\17\10\5\10\17\10\17\10\17\10\17\10\17\10\17\10\17\10\17\10\5\10\17\10\17\10\5\10\5\4\16\4\16\4\4\4\8" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
