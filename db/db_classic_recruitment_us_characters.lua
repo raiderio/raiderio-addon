dfd4bbee6ac5ef7d47eb0c551a9012d1f6cda9ee
Jah Raphael <jahraphael@yahoo.com>
2026-01-14 06:10:13 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-01-13T06:07:44Z",numCharacters=14,db={}}
+local provider={name=...,data=3,region="us",date="2026-01-14T06:09:11Z",numCharacters=18,db={}}
 local F
 
 F = function() provider.db["Galakras"]={0,"Warpando"} end F()
