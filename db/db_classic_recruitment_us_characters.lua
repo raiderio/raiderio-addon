83addc1051031864835ee15664aadff2fa28b99e
Jah Raphael <jahraphael@yahoo.com>
2026-01-25 06:13:31 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,11 +2,10 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-01-24T06:09:34Z",numCharacters=33,db={}}
+local provider={name=...,data=3,region="us",date="2026-01-25T06:12:48Z",numCharacters=27,db={}}
 local F
 
-F = function() provider.db["Pagle"]={0,"Arborious","Blinkinblake","Finatae","Holysesher","Lessholy","Namereported","Nanajan","Powersticks","Slashme","Touchofkarma","Uno","Wethealussy","Whozewhatzit","Xdxdxdxdxdxd"} end F()
-F = function() provider.db["Galakras"]={28,"Warpando"} end F()
+F = function() provider.db["Pagle"]={0,"Arborious","Finatae","Holysesher","Lessholy","Namereported","Nanajan","Powersticks","Slashme","Touchofkarma","Uno","Wethealussy","Whozewhatzit","Xdxdxdxdxdxd"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
