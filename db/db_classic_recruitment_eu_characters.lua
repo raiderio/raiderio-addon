882d1df199dc9bda9eb46add519d99d9476b262a
Jah Raphael <jahraphael@yahoo.com>
2026-02-04 06:07:11 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,12 +2,12 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-02-03T06:07:16Z",numCharacters=27,db={}}
+local provider={name=...,data=3,region="eu",date="2026-02-04T06:06:56Z",numCharacters=24,db={}}
 local F
 
-F = function() provider.db["Hoptallus"]={0,"Arizara","Eruveena","Feronya","Ludidoktor","Ludidoktorr","Ludidokttor","Nacugivara","Nikotako","Noshecant","Psyvise","Rualan"} end F()
-F = function() provider.db["Norushen"]={22,"Seruss","Tonzil"} end F()
-F = function() provider.db["Everlook"]={26,"Captntobi","Chugganaut","Imperatvs"} end F()
+F = function() provider.db["Hoptallus"]={0,"Arizara","Eruveena","Feronya","Ludidoktor","Nikotako","Psyvise","Rualan"} end F()
+F = function() provider.db["Norushen"]={14,"Seruss","Tonzil"} end F()
+F = function() provider.db["Everlook"]={18,"Captntobi","Chugganaut","Imperatvs"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-02-03T06:07:16Z",numCharacters=27,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2026-02-04T06:06:56Z",numCharacters=24,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 22
-F = function() provider.lookup[1] = "\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9" end F()
+-- chunk size: 24
+F = function() provider.lookup[1] = "\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9\10\9" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
