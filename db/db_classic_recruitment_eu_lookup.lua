fed42831f4220ec52683b58171bc174a0f1d5b58
Jah Raphael <jahraphael@yahoo.com>
2026-02-18 06:09:37 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-02-17T06:09:00Z",numCharacters=50,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2026-02-18T06:09:01Z",numCharacters=45,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 72
-F = function() provider.lookup[1] = "\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\17\10\5\10\17\10\17\10\17\10\17\10\17\10\17\10\17\10\17\10\5\10\17\10\17\4\4" end F()
+-- chunk size: 64
+F = function() provider.lookup[1] = "\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\17\10\5\10\17\10\17\10\17\10\17\10\5\10\17\10\17\4\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,10 +2,10 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-02-17T06:09:00Z",numCharacters=50,db={}}
+local provider={name=...,data=3,region="us",date="2026-02-18T06:09:01Z",numCharacters=45,db={}}
 local F
 
-F = function() provider.db["Pagle"]={0,"Blinkinblake","Finatae","Holysesher","Kísara","Lessholy","Namereported","Nanajan","Reáven","Slashme","Touchofkarma","Uno","Wethealussy","Whozewhatzit","Xdxdxdxdxdxd"} end F()
+F = function() provider.db["Pagle"]={0,"Finatae","Holysesher","Kísara","Lessholy","Namereported","Nanajan","Reáven","Slashme","Touchofkarma","Uno","Wethealussy","Whozewhatzit","Xdxdxdxdxdxd"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
