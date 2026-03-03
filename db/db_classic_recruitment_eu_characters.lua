c83238870a754b6405b3e15fb210b6ad6c17fabd
Jah Raphael <jahraphael@yahoo.com>
2026-03-03 06:07:05 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,13 +2,13 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-03-02T06:07:17Z",numCharacters=32,db={}}
+local provider={name=...,data=3,region="eu",date="2026-03-03T06:06:35Z",numCharacters=31,db={}}
 local F
 
-F = function() provider.db["Everlook"]={0,"Bâînê","Büffelhüftè","Czarodejnica","Dosy","Drangosh","Draufgehn","Drride","Hamulcvr","Jommey","Kaelthoras","Mallamalla","Molax","Naeq","Shokie","Siqras","Vylariana","Wildgulasch","Wildhearts","Wototem"} end F()
-F = function() provider.db["Hoptallus"]={38,"Arizara","Drdottington","Eruveena","Feronya","Ludidoktor","Nikotako","Psyvise","Rualan"} end F()
-F = function() provider.db["Norushen"]={54,"Doybear","Dythu","Synomnomnom","Taibalt"} end F()
-F = function() provider.db["Garalon"]={62,"Nylock"} end F()
+F = function() provider.db["Everlook"]={0,"Bâînê","Czarodejnica","Dosy","Drangosh","Draufgehn","Drride","Hamulcvr","Jommey","Kaelthoras","Mallamalla","Molax","Naeq","Shokie","Siqras","Vylariana","Wildgulasch","Wildhearts","Wototem"} end F()
+F = function() provider.db["Hoptallus"]={36,"Arizara","Drdottington","Eruveena","Feronya","Ludidoktor","Nikotako","Psyvise","Rualan"} end F()
+F = function() provider.db["Norushen"]={52,"Doybear","Dythu","Synomnomnom","Taibalt"} end F()
+F = function() provider.db["Garalon"]={60,"Nylock"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-03-02T06:07:17Z",numCharacters=32,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2026-03-03T06:06:35Z",numCharacters=31,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 64
-F = function() provider.lookup[1] = "\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\17\10\5\10\17\10\17\10\17\10\17\10\17\10\17\10\5\10\5\10\5\10\5\4\4" end F()
+-- chunk size: 62
+F = function() provider.lookup[1] = "\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\21\10\17\10\5\10\17\10\17\10\17\10\17\10\17\10\17\10\5\10\5\10\5\10\5\4\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
