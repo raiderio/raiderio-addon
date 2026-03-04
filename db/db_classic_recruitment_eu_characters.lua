7f114ddce4ac7c24b3ada8e3cf022f12dcb03a8e
Jah Raphael <jahraphael@yahoo.com>
2026-03-04 06:06:55 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-03-03T06:06:35Z",numCharacters=31,db={}}
+local provider={name=...,data=3,region="eu",date="2026-03-04T06:06:25Z",numCharacters=31,db={}}
 local F
 
 F = function() provider.db["Everlook"]={0,"Bâînê","Czarodejnica","Dosy","Drangosh","Draufgehn","Drride","Hamulcvr","Jommey","Kaelthoras","Mallamalla","Molax","Naeq","Shokie","Siqras","Vylariana","Wildgulasch","Wildhearts","Wototem"} end F()
--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-03-03T06:06:35Z",numCharacters=31,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2026-03-04T06:06:25Z",numCharacters=31,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 62
