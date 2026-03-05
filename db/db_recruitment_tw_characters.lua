a1313043f6da50eca9ff46e119f2061637dbc7e0
Jah Raphael <jahraphael@yahoo.com>
2026-03-05 13:50:48 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_tw_characters.lua
+++ db/db_recruitment_tw_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="tw",date="2026-03-02T08:55:19Z",numCharacters=29324,db={}}
+local provider={name=...,data=3,region="tw",date="2026-03-05T13:49:21Z",numCharacters=28150,db={}}
 local F
 
 F = function() provider.db["暗影之月"]={0,"Piperfs"} end F()
