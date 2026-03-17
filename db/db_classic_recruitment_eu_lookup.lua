2d76484b86fa437cc7382c008f37099cac35c62a
Jah Raphael <jahraphael@yahoo.com>
2026-03-17 06:06:22 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-03-16T06:06:15Z",numCharacters=18,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2026-03-17T06:06:13Z",numCharacters=18,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 34
