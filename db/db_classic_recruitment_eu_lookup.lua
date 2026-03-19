b5b323e3e861015fbce5a55f5f729a6579a8be73
Jah Raphael <jahraphael@yahoo.com>
2026-03-19 06:06:23 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_lookup.lua
+++ db/db_classic_recruitment_eu_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-03-18T06:06:45Z",numCharacters=18,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="eu",date="2026-03-19T06:06:12Z",numCharacters=18,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 34
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-03-18T06:06:45Z",numCharacters=18,db={}}
+local provider={name=...,data=3,region="us",date="2026-03-19T06:06:12Z",numCharacters=18,db={}}
 local F
 
 F = function() provider.db["Nazgrim"]={0,"Afksham"} end F()
