8cb94e10a4c9615295450853bbefeaa181ec6338
Jah Raphael <jahraphael@yahoo.com>
2026-03-25 06:52:05 +0000

    [Raider.IO] Database Refresh

--- db/db_mythicplus_kr_characters.lua
+++ db/db_mythicplus_kr_characters.lua
@@ -2,10 +2,10 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=1,region="kr",date="2026-03-24T06:44:24Z",currentSeasonId=0,numCharacters=7894,keystoneMilestoneLevels={15,12,10,7,4,2},db={}}
+local provider={name=...,data=1,region="kr",date="2026-03-25T06:45:34Z",currentSeasonId=0,numCharacters=152916,keystoneMilestoneLevels={15,12,10,7,4,2},db={}}
 local F
 
-F = function() provider.db["아즈샤라"]={0,"Carmin","Ctrl","Famous","Fastforward","Imitation","Karmin","Line","Schnauzer","Sketch","국왕상스","귀천마객","귀천마삼","기천마객","넨멸","넨법","넨성","넨전","넨주","넨죽","넨흑","미간미간","민농","빨간머리고아","사제민농","절개절개","절단절단","직술","직양","직용","직우","직월","직풍","직향","폭테상스","호랑이"} end F()
+F = function() provider.db["아즈샤라"]={0,"Carmin","Ctrl","Famous","Fastforward","Imitation","Karmin","Line","Schnauzer","Sketch","국왕상스","귀천마삼","기천마객","넨냥","넨멸","넨법","넨성","넨전","넨주","넨죽","넨흑","미간미간","민농","빨간머리고아","사제민농","절개절개","절단절단","직술","직양","직용","직우","직월","직풍","직향","폭테상스","호랑이"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
--- db/db_mythicplus_tw_characters.lua
+++ db/db_mythicplus_tw_characters.lua
@@ -2,17 +2,17 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=1,region="tw",date="2026-03-24T06:44:24Z",currentSeasonId=0,numCharacters=7894,keystoneMilestoneLevels={15,12,10,7,4,2},db={}}
+local provider={name=...,data=1,region="tw",date="2026-03-25T06:45:34Z",currentSeasonId=0,numCharacters=152916,keystoneMilestoneLevels={15,12,10,7,4,2},db={}}
 local F
 
 F = function() provider.db["眾星之子"]={0,"夜嵐緋雪","龍蝦小魚卵"} end F()
 F = function() provider.db["暗影之月"]={60,"緋鳳輓歌"} end F()
 F = function() provider.db["銀翼要塞"]={90,"Magicstar","天嵐彗星"} end F()
-F = function() provider.db["日落沼澤"]={150,"凝川"} end F()
-F = function() provider.db["語風"]={180,"Snowynight"} end F()
-F = function() provider.db["水晶之刺"]={210,"憨豆大媽","被強煎的蛋","麥能爺"} end F()
-F = function() provider.db["夜空之歌"]={300,"Montblanc","Mythicat","Pinkwarrior","一萬年的雪","床邊目擊者","烽煙肆起","版築雉堞之殷","牛郎店男模","舒服嗎"} end F()
-F = function() provider.db["冰霜之刺"]={570,"橘子班長"} end F()
+F = function() provider.db["日落沼澤"]={150,"凝川","憨豆大媽"} end F()
+F = function() provider.db["語風"]={210,"Snowynight"} end F()
+F = function() provider.db["水晶之刺"]={240,"憨豆大媽","被強煎的蛋","麥能爺"} end F()
+F = function() provider.db["夜空之歌"]={330,"Montblanc","Mythicat","Pinkwarrior","一萬年的雪","床邊目擊者","烽煙肆起","版築雉堞之殷","牛郎店男模","舒服嗎"} end F()
+F = function() provider.db["冰霜之刺"]={600,"橘子班長"} end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
