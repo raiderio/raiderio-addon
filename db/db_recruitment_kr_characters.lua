c4df24c01d0d2e19dde739477e7aa5ac72dceeb0
Jah Raphael <jahraphael@yahoo.com>
2026-04-01 07:17:38 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2026-03-30T07:00:44Z",numCharacters=29658,db={}}
+local provider={name=...,data=3,region="kr",date="2026-04-01T07:12:32Z",numCharacters=30487,db={}}
 local F
 
 F = function() provider.db["아즈샤라"]={0,"마스트리안","아스리엘문레이"} end F()
