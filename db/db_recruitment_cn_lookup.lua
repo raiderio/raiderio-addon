f2830c70d01c79f2c86987e6667611e312cad94e
Jah Raphael <jahraphael@yahoo.com>
2026-04-03 07:24:12 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_lookup.lua
+++ db/db_recruitment_cn_lookup.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-04-02T07:12:03Z",numCharacters=31023,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="cn",date="2026-04-03T07:18:12Z",numCharacters=31221,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
 -- chunk size: 50
--- db/db_recruitment_kr_characters.lua
+++ db/db_recruitment_kr_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="kr",date="2026-04-02T07:12:03Z",numCharacters=31023,db={}}
+local provider={name=...,data=3,region="kr",date="2026-04-03T07:18:12Z",numCharacters=31221,db={}}
 local F
 
 F = function() provider.db["아즈샤라"]={0,"마스트리안","아스리엘문레이"} end F()
