138289132f869a1a091c858e46747fb49369283e
Jah Raphael <jahraphael@yahoo.com>
2026-04-15 07:39:46 +0000

    [Raider.IO] Database Refresh

--- db/db_recruitment_cn_characters.lua
+++ db/db_recruitment_cn_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2026 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="cn",date="2026-04-14T07:26:26Z",numCharacters=34278,db={}}
+local provider={name=...,data=3,region="cn",date="2026-04-15T07:29:30Z",numCharacters=34328,db={}}
 local F
 
 F = function() provider.db["无尽之海"]={0,"Greyghost","Moondance","Pen","Tenpen","一点二十丸","丨释怀","乄德","乔乔小弟","九点二十一","可怜德古丹","周哲伦","大废止者","整活大師","札皮","渣皮","無料案内人","独轶望江楼","电击治疗丶","章鱼哥的邻居","轧皮","释丨怀","阿蒙内特","骑骑丶"} end F()
