dce5da08647a813022ddff015f30845c3ed16661
Jah Raphael <jahraphael@yahoo.com>
2026-05-28 08:07:24 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_us_lookup.lua
+++ db/db_classic_recruitment_us_lookup.lua
@@ -2,11 +2,11 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-05-27T08:06:55Z",numCharacters=4,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
+local provider={name=...,data=3,region="us",date="2026-05-28T08:06:38Z",numCharacters=5,lookup={},recordSizeInBytes=2,encodingOrder={0,1,3}}
 local F
 
--- chunk size: 6
-F = function() provider.lookup[1] = "\4\4\4\4\4\4" end F()
+-- chunk size: 8
+F = function() provider.lookup[1] = "\4\4\4\4\4\4\4\4" end F()
 
 F = nil
 RaiderIO.AddProvider(provider)
