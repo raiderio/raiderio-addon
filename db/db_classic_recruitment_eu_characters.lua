d5af487cf9f058f4e6b0e9d0db9e9ea3d1434ed4
Jah Raphael <jahraphael@yahoo.com>
2026-06-10 08:07:41 +0000

    [Raider.IO] Classic Database Refresh

--- db/db_classic_recruitment_eu_characters.lua
+++ db/db_classic_recruitment_eu_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="eu",date="2026-06-09T08:06:37Z",numCharacters=2,db={}}
+local provider={name=...,data=3,region="eu",date="2026-06-10T08:07:25Z",numCharacters=2,db={}}
 local F
 
 F = function() provider.db["MirageRaceway"]={0,"Magopok"} end F()
--- db/db_classic_recruitment_us_characters.lua
+++ db/db_classic_recruitment_us_characters.lua
@@ -2,7 +2,7 @@
 -- Copyright (c) 2025 by RaiderIO, Inc.
 -- All rights reserved.
 --
-local provider={name=...,data=3,region="us",date="2026-06-09T08:06:37Z",numCharacters=2,db={}}
+local provider={name=...,data=3,region="us",date="2026-06-10T08:07:25Z",numCharacters=2,db={}}
 local F
 
 F = function() provider.db["Galakras"]={0,"Hàney"} end F()
